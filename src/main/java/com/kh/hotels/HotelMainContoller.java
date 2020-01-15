808c2c02d215fb13fb29a644bf0a4afa3f146d02
SJ <SJ@DESKTOP-TO01RGI>
2020-01-15 18:12:39 +0900

    호텔페이지 작업중...

--- src/main/java/com/kh/hotels/HotelMainContoller.java
+++ src/main/java/com/kh/hotels/HotelMainContoller.java
@@ -2,19 +2,33 @@ package com.kh.hotels;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HotelMainContoller {
 
+//	@GetMapping("goMain.hadmin")
+//	public String showHotelAdmin() {
+//		
+//		return "hoteladmin/main/main";
+//	}
+	
 	@GetMapping("goMain.hmain")
 	public String showHotelMain() {
 		
 		return "hotelmain/main/main";
 	}
-	@GetMapping("goMain.hadmin")
-	public String showHotelAdmin() {
+	
+	@RequestMapping("goRooms.hmain")
+	public String showHotelRooms() {
 		
-		return "hoteladmin/main/main";
+		return "hotelmain/rooms/roomList";
+	}
+	
+	@RequestMapping("roomdetail.hmain")
+	public String showHotelRoomsDetail() {
+		
+		return "hotelmain/rooms/roomDetail";
 	}
 	
 }
